b5b3620d3f95f7e3894010d43722fe6cbf1f14e6
vlad0-0 <54848408+vlad0-0@users.noreply.github.com>
2019-12-03 13:19:21 +0300

    Update 14.3.py

--- 14.3.py
+++ 14.3.py
@@ -1,10 +1,12 @@
 n = int(input())
 while n<2:
     n = int(input())
-b = ""
+b = 0
 a = []
+c = True
 for i in range(n):
     a.append(input())
-    if i%2 != 0 and (a[i] < b or b == ""):
+    if i%2 != 0 and (a[i] < b or c):
         b = a[i]
+        c = False
 print(b)
